94adbe88eed72b1578c4906e66fe81d2b579d279
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-05-18 13:48:34 +0300

    Problem 3 added

---
+++ 01_Syntax_Functions_Lab/03_largest_number.js
@@ -0,0 +1,7 @@
+function largest(number1, number2, number3) {
+    let largestNumber = Math.max(number1, number2, number3);
+    console.log(`The largest number is ${largestNumber}.`)
+}
+
+largest(5, -3, 16);
+largest(-3, -5, -22.5);
