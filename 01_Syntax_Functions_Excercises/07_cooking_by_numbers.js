9c1ba64839b1efad1593a35ca216bd768c5384b2
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-05-19 16:02:00 +0300

    Problem 7 added

---
+++ 01_Syntax_Functions_Excercises/07_cooking_by_numbers.js
@@ -0,0 +1,23 @@
+function cookingByNumbers(number, operation1, operation2, operation3, operation4, operation5) {
+    let result = number;
+
+    let operations = [operation1, operation2, operation3, operation4, operation5];
+
+    for (op of operations) {
+        if (op == 'chop') {
+            result /= 2;
+        } else if (op == 'dice') {
+            result = Math.sqrt(result);
+        } else if (op == 'spice') {
+            result += 1;
+        } else if (op == 'bake') {
+            result *= 3;
+        } else if (op == 'fillet') {
+            result *= 0.8;
+        }
+        console.log(result);
+    }
+}
+
+cookingByNumbers('32', 'chop', 'chop', 'chop', 'chop', 'chop');
+cookingByNumbers('9', 'dice', 'spice', 'chop', 'bake', 'fillet');
