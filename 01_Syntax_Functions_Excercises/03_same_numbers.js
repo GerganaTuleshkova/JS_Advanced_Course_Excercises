ef99f82f1cd316739f218fac306dd9c866c1ec6c
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-05-19 14:51:50 +0300

    Problem 3 added

---
+++ 01_Syntax_Functions_Excercises/03_same_numbers.js
@@ -0,0 +1,18 @@
+function hasSameNumbers(number) {
+    let numberAsString = number.toString();
+    let digit = numberAsString[0];
+    let result = true;
+    let sum = 0;
+    for (ch of numberAsString.slice(1)) {
+        if (ch != digit) {
+            result = false;
+        }
+        sum += Number(ch);
+    }
+    console.log(result);
+    console.log(sum);
+
+}
+
+hasSameNumbers(22222222);
+hasSameNumbers(12334);
