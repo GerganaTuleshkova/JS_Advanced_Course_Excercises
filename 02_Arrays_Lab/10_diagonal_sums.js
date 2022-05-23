3768763484fcdb17fb5c362e6eb5310b9d284423
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-05-23 16:29:41 +0300

    Problem 10 added

---
+++ 02_Arrays_Lab/10_diagonal_sums.js
@@ -0,0 +1,15 @@
+function diagonalSums(matrix) {
+    let mainDiagonalSum = 0;
+    let secondaryDiagonalSum = 0;
+    for (let rowIndex = 0; rowIndex < matrix.length; rowIndex++) {
+        mainDiagonalSum += matrix[rowIndex][rowIndex];
+        secondaryDiagonalSum += matrix[rowIndex][matrix.length - 1 - rowIndex]
+    }
+    console.log(mainDiagonalSum, secondaryDiagonalSum)
+}
+
+diagonalSums([[20, 40],[10, 60]])
+diagonalSums([[3, 5, 17],
+    [-1, 7, 14],
+    [1, -8, 89]]
+   )
