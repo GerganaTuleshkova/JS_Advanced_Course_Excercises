e68bf0b6ff20ab5c375880153dd2aff6b4884bb0
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-05-23 15:17:32 +0300

    Problem 8 added

--- 02_Arrays_Lab/08_process_odd_positions.js
+++ 02_Arrays_Lab/08_process_odd_positions.js
@@ -1,10 +1,9 @@
 function processOddPositions(numbers) {
-    const result = numbers.filter((n, i) => i % 2);
-    const doubled = result.map(n => n * 2)
-    
+    const result = numbers.filter((_, i) => i % 2);
+    const doubled = result.map(n => n * 2);
     doubled.reverse();
-    return doubled.join(' ')
+    return doubled.join(' ');
 }
 
-console.log(processOddPositions([10, 15, 20, 25]))
-console.log(processOddPositions([3, 0, 10, 4, 7, 3]))
+console.log(processOddPositions([10, 15, 20, 25]));
+console.log(processOddPositions([3, 0, 10, 4, 7, 3]));
