cc0bb0a905c12e17de9f644966cff239b88d4bb9
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-05-24 15:02:23 +0300

    Problem 7 added

---
+++ 02_Arrays_Excercises/07_sorting_numbers.js
@@ -0,0 +1,17 @@
+function sortingNumbers(numbers) {
+    let sortedNumbers = numbers.sort((a, b) => a - b);
+    const iterrations = numbers.length;
+    console.log(numbers)
+    const result = [];
+    for (let i = 0; i < iterrations; i++) {
+        if (i % 2 == 0) {
+            result.push(sortedNumbers.shift())
+        } else {
+            result.push(sortedNumbers.pop())
+        }
+    }
+
+    return result
+}
+
+console.log(sortingNumbers([1, 65, 3, 52, 48, 63, 31, -3, 18, 56]));
