0161e224fae3beb4234efe05169e66b5f6665903
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-05-26 13:05:25 +0300

    Updated solution

--- 02_Arrays_Excercises/06_list_of_names.js
+++ 02_Arrays_Excercises/06_list_of_names.js
@@ -1,8 +1,8 @@
 function listOfNames(names) {
     let sortedNames = names.sort((a, b) => a.localeCompare(b));
     for (let i = 0; i < sortedNames.length; i++) {
-        console.log(`${i+1}.${sortedNames[i]}`);
-    }    
+        console.log(`${i + 1}.${sortedNames[i]}`);
+    }
 }
 
 listOfNames(["John", "Bob", "Christina", "Ema"])
