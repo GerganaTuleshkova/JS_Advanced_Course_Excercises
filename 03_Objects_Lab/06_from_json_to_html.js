193949cc33ae44b3983d298d94cf4223d7b8ac00
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-05-27 09:24:31 +0300

    Problem 6 added

--- 03_Objects_Lab/06_from_json_to_html.js
+++ 03_Objects_Lab/06_from_json_to_html.js
@@ -1,21 +1,18 @@
+
 function fromJSONToHTMLTable(json) {
 
     let arr = JSON.parse(json);
 
-    console.log(arr);
     let outputArr = ["<table>"];
 
     outputArr.push(makeKeyRow(arr));
-
-    console.log(outputArr);
-
     arr.forEach((obj) => outputArr.push(makeValueRow(obj)));
     outputArr.push("</table>");
 
     function makeKeyRow(arr) {
         let result = '<tr>';
         for (let key of Object.keys(arr[0])) {
-            result += `<th>${key}</th>`;
+            result += `<th>${escapeHTML(key)}</th>`;
         }
         result += '</tr>';
         return result;
@@ -24,7 +21,7 @@ function fromJSONToHTMLTable(json) {
         let result = '<tr>';
 
         for (let value of Object.values(obj)) {
-            result += `<td>${value}</td>`;
+            result += `<td>${escapeHTML(value)}</td>`;
         }
 
 
@@ -32,7 +29,19 @@ function fromJSONToHTMLTable(json) {
         return result;
 
     };
-    // function escapeHtml(value) { };
+
+    function escapeHTML(value) {
+        return value
+            .toString()
+            .replace(/&/g, '&amp;')
+            .replace(/</g, '&lt;')
+            .replace(/>/g, '&gt;')
+            .replace(/"/g, '&quot;')
+            .replace(/'/g, '&#39;');
+    }
+
+
+
     // print the result
     console.log(outputArr.join('\n'));
 }
