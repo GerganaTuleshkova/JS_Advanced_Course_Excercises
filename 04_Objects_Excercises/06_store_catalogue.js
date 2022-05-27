1903965b85ec925ac111ed2ae864abc9e0a374b3
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-05-27 09:58:28 +0300

    Problem  6 added

---
+++ 04_Objects_Excercises/06_store_catalogue.js
@@ -0,0 +1,45 @@
+function createSortedCatalogue(arrOfStrings) {
+    const catalogue = [];
+
+    for (let str of arrOfStrings) {
+        let [name, price] = str.split(' : ');
+        price = Number(price);
+        catalogue.push([name, price]);
+    }
+
+    // const sortedCatalogue = [];
+    // for (let [name, price] of Object.entries(catalogue)) {
+    //     sortedCatalogue.push([name, price]);
+    // }
+    let sortedCatalogueArray = catalogue.sort(([a], [b]) => (a).localeCompare(b));
+
+    const result = {}
+
+    for (let element of sortedCatalogueArray) {
+        let firstLetter = element[0][0].toUpperCase()
+        if (!result[firstLetter]) {
+             result[firstLetter] = [element];
+        } else {
+            result[firstLetter].push(element)
+        }       
+    }
+
+    for (let letter of Object.keys(result)) {
+        console.log(letter);
+        for (let product of result[letter]) {
+            console.log(`  ${product[0]}: ${product[1]}`)
+        }
+}
+    }
+    
+
+createSortedCatalogue(['Banana : 2',
+"Rubic's Cube : 5",
+'Raspberry P : 4999',
+'Rolex : 100000',
+'Rollon : 10',
+'Rali Car : 2000000',
+'Pesho : 0.000001',
+'Barrel : 10']
+
+)
