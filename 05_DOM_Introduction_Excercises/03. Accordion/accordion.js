880d6c6fc174be862fb2b911a101d95066fbb107
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-05-30 19:32:32 +0300

    Solution updated

--- 05_DOM_Introduction_Excercises/03. Accordion/accordion.js
+++ 05_DOM_Introduction_Excercises/03. Accordion/accordion.js
@@ -7,6 +7,13 @@ function toggle() {
         document.getElementById('extra').style.display = 'block'
     } else if (buttonText == 'Less') {
         document.getElementsByClassName('button')[0].textContent = 'More';
-        document.getElementById('extra').style.display = 'none'
+        document.getElementById('extra').style.display = ''
     }
+
+    // second option
+    // let button = document.querySelector(".button");
+    // let text = document.querySelector("#extra");
+
+    // text.style.display = text.style.display == "block" ? "none" : "block";
+    // button.textContent = button.textContent == "More" ? "Less" : "More";
 }
