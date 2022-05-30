f7f6d9438c8324ab5d4bddf5037491965b2a0d34
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-05-30 14:41:53 +0300

    Problem 2 added

---
+++ 05_DOM_Introduction_Excercises/02. Pascal or Camel Case/solution.js
@@ -0,0 +1,24 @@
+function solve() {
+  const givenText = document.getElementById('text').value;
+  const words = givenText.split(' ')
+  const namingConvention = document.getElementById('naming-convention').value;
+  let result = '';
+
+  function  capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+  }
+
+  if (namingConvention == 'Camel Case') {
+    result += words.shift().toLowerCase();
+    const restOfWords = words.map(w => capitalizeFirstLetter(w));
+    result += restOfWords.join('');
+  } else if (namingConvention == 'Pascal Case') {
+    const restOfWords = words.map(w => capitalizeFirstLetter(w));
+    result += restOfWords.join('');
+  } else {
+    result = 'Error!'
+  }
+
+  document.getElementById('result').textContent = result;
+
+}
