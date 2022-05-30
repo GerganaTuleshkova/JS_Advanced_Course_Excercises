155cc417c9696e517b05db69380e5f030ff25132
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-05-30 12:01:50 +0300

    Mofified

--- 05_DOM_Introduction_Lab/01.Edit Element/EditElement.js
+++ 05_DOM_Introduction_Lab/01.Edit Element/EditElement.js
@@ -1,3 +1,5 @@
-function editElement() {
-    // TODO
+function editElement(element, match, replacer) {
+    let elementText = element.textContent;
+    let newText = elementText.split(match).join(replacer);
+    element.textContent = newText;
 }
--- 05_DOM_Introduction_Lab/02.Collect List Items/CollectListItems.js
+++ 05_DOM_Introduction_Lab/02.Collect List Items/CollectListItems.js
@@ -1,14 +1,8 @@
 function extractText() {
-    // const resultText = ''
-    const listItems = Array.from(document.getElementsByTagName("li"));
+    const listItems = Array.from(document.getElementsByTagName('li'));
 
     let itemsTexts = listItems.map(e => e.textContent);
-    console.log(itemsTexts)
     const resultText = itemsTexts.join('\n');
-    console.log(resultText);
 
     document.getElementById('result').textContent = resultText;
-
-    
-
 }
