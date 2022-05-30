1389cf27e61b39944822db130f62fef5ea2c42e0
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-05-30 20:13:15 +0300

    Solution updated

--- 05_DOM_Introduction_Excercises/05. Table - Search Engine/solution.js
+++ 05_DOM_Introduction_Excercises/05. Table - Search Engine/solution.js
@@ -2,15 +2,17 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
+      //remove classes from table rows
       for (element of document.querySelectorAll('tr')) {
-         element.classList.remove('select')
-
+         element.classList.remove('select');
       }
+
       const searchedText = document.getElementById('searchField').value;
+      
       for (element of document.querySelectorAll('tr > td')) {
-         console.log(element)
          if (element.textContent.includes(searchedText)) {
-            element.parentElement.className = 'select'
+            element.parentElement.className = 'select';
+            document.getElementById('searchField').value = ''
          }
       }
    }
