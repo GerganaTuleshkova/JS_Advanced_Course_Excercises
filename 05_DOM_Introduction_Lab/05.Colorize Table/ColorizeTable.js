3c16d66db3305be10576daecef9a2a799b058d69
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-05-31 12:10:10 +0300

    Updated solution

--- 05_DOM_Introduction_Lab/05.Colorize Table/ColorizeTable.js
+++ 05_DOM_Introduction_Lab/05.Colorize Table/ColorizeTable.js
@@ -1,6 +1,9 @@
 function colorize() {
     const rows = document.getElementsByTagName('tr');
 
+    // could use:
+    // document.querySelectorAll('tr:nth-child(2n)')
+
     for (i = 1; i < rows.length; i += 2) {
         rows[i].style.background = 'Teal';
     }
