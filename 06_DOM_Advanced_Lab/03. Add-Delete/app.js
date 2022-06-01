0eb79d3369d90c2ce7247be6a39db7a4d48fb6d1
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-06-01 19:36:37 +0300

    Problem 3 added

--- 06_DOM_Advanced_Lab/03. Add-Delete/app.js
+++ 06_DOM_Advanced_Lab/03. Add-Delete/app.js
@@ -1,3 +1,21 @@
 function addItem() {
-    //TODO...
+    let input = document.getElementById('newItemText');
+    let ulElement = document.getElementById('items')
+    let newItem = document.createElement('li');
+    newItem.textContent = input.value;
+    let deleteLink = document.createElement('a');
+    deleteLink.textContent = '[Delete]';
+    deleteLink.href = '#';
+
+    deleteLink.addEventListener('click', onDelete);
+
+    newItem.appendChild(deleteLink);
+    ulElement.appendChild(newItem);
+    function onDelete(event) {
+        event.target.parentElement.remove();
+        console.log(event);
+    }
+
+    input.value = '';
 }
+
