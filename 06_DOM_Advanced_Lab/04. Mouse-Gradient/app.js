8ac9200f58c35cffa95bc347e7adc7573fe97689
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-06-01 20:22:29 +0300

    Problem 4 added

--- 06_DOM_Advanced_Lab/04. Mouse-Gradient/app.js
+++ 06_DOM_Advanced_Lab/04. Mouse-Gradient/app.js
@@ -1,3 +1,9 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    let gradient = document.getElementById('gradient');
+    gradient.addEventListener('mousemove', onMouseMove);
+
+    function onMouseMove(event) {
+        let offsetInPercents = Math.floor(event.offsetX / gradient.clientWidth * 100);
+        document.getElementById('result').textContent = `${offsetInPercents}%`;
+    }
 }
