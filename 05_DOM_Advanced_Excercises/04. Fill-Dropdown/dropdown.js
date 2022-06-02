16425ef4db1bb2f18a537a637248106315d1f8b3
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-06-02 16:27:31 +0300

    Problem 4 added

--- 05_DOM_Advanced_Excercises/04. Fill-Dropdown/dropdown.js
+++ 05_DOM_Advanced_Excercises/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,15 @@
 function addItem() {
-    console.log('TODO:...');
+    let textElement = document.getElementById('newItemText');
+    let valueElement = document.getElementById('newItemValue');
+
+    let newOption = document.createElement('option')
+
+    newOption.textContent = textElement.value;
+    newOption.value = valueElement.value;
+    document.getElementById('menu').appendChild(newOption);
+
+    textElement.value = '';
+    valueElement.value = '';
+
+    console.log(newOption)
 }
