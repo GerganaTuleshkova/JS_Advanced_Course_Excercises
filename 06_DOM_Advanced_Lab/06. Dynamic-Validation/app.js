06464bf918b344ad0451996868882b336f80f6f2
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-06-02 10:18:49 +0300

    Problem 6 added

--- 06_DOM_Advanced_Lab/06. Dynamic-Validation/app.js
+++ 06_DOM_Advanced_Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    console.log('TODO:...');
+    let inputElement = document.getElementById('email');
+    inputElement.addEventListener('change', onChange);
+
+    function onChange(event) {
+        inputElement.className = '';
+        let regex = /[a-z]+@[a-z]+.[a-z]+/g;
+        let isValid = regex.exec(inputElement.value);
+        if (!isValid) {
+            inputElement.className = 'error';
+        } else {
+            inputElement.value = '';
+        }
+    }
 }
