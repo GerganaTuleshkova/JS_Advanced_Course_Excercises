2275e4fde2c33808269fa522e975187f8807a138
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-06-02 16:16:54 +0300

    Problem 3 added

--- 05_DOM_Advanced_Excercises/02. Time-Converter/solution.js
+++ 05_DOM_Advanced_Excercises/02. Time-Converter/solution.js
@@ -1,4 +1,60 @@
 function attachEventsListeners() {
 
-    console.log('TODO:...');
+    let daysInputElement = document.getElementById('days');
+    let hoursInputElement = document.getElementById('hours');
+    let minutesInputElement = document.getElementById('minutes');
+    let secondsInputElement = document.getElementById('seconds');
+
+    daysInputElement.value = 0
+    hoursInputElement.value = 0;
+    minutesInputElement.value = 0;
+    secondsInputElement.value = 0;
+
+    let daysForConversion = 0;
+
+    let buttonElements = Array.from(document.querySelectorAll('input[type="button"]'))
+    .forEach( e => e.addEventListener('click', onClick));
+
+    let inputElements = Array.from(document.querySelectorAll('input[type="text"]'))
+
+    // console.log(buttonElements.length)
+
+    // daysInputElement.nextElementSibling.addEventListener('click', daysToOthers);
+    // hoursInputElement.nextElementSibling.addEventListener('click', hoursToOthers)
+    // minutesInputElement.nextElementSibling.addEventListener('click', minutesToOthers)
+    // secondsInputElement.nextElementSibling.addEventListener('click', secondsToOthers)
+
+    // function daysToOthers(event) {
+    //     let days = Number(daysInputElement.value);
+    //     let hours = days * 24;
+    //     let minutes = days * 24 * 60;
+    //     let seconds = days * 24 * 60 * 60;
+
+    //     hoursInputElement.value = hours;
+    //     minutesInputElement.value = minutes;
+    //     secondsInputElement.value = seconds
+    // }
+
+    // function hoursToOthers(event) {
+    //     let hours = Number(hoursInputElement.value);
+    //     let days = hours / 24;
+    //     let minutes = days * 24 * 60;
+    //     let seconds = days * 24 * 60 * 60;
+
+    //     daysInputElement.value = days;
+    //     minutesInputElement.value = minutes;
+    //     secondsInputElement.value = seconds
+    // }
+
+    function onClick(event) {
+        //find which input was changed
+        for (let el of inputElements) {
+            let elValue = Number(el.value)
+            if (elValue != 0 ) {
+                
+            }
+        }
+
+
+    }
 }
--- 05_DOM_Advanced_Excercises/03. Locked-Profile/app.js
+++ 05_DOM_Advanced_Excercises/03. Locked-Profile/app.js
@@ -1,3 +1,23 @@
 function lockedProfile() {
-    console.log('TODO...')
+    let parentMainElement = document.getElementById('main');
+    parentMainElement.addEventListener('click', onClick, true);
+
+    function onClick(event) {
+        if (event.target.nodeName == 'BUTTON') {
+            if (!isLocked(event)) {
+                let hiddenFields = event.target.parentElement.querySelector('div')
+                if (event.target.textContent == 'Show more') {
+                    hiddenFields.style.display = 'block';
+                    event.target.textContent = 'Hide it';
+                } else {
+                    hiddenFields.style.display = 'none';
+                    event.target.textContent = 'Show more';
+                }
+            }
+        }
+    }
+
+    function isLocked(event) {
+        return (event.target.parentElement.querySelector('input[value="lock"]').checked);
+    }
 }
