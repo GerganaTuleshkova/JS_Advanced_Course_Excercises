64d4c63d2d44e3481f5640dcefe6b4dcc7f84f59
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-06-02 14:52:48 +0300

    Problem 1 added

--- 05_DOM_Advanced_Excercises/01. Sections/script.js
+++ 05_DOM_Advanced_Excercises/01. Sections/script.js
@@ -1,3 +1,23 @@
 function create(words) {
-   console.log('TODO:...');
+
+   let parentDivElement = document.getElementById('content');
+   parentDivElement.addEventListener('click', onClick);
+
+   function createElement(word) {
+      let element = document.createElement('div');
+      let pElement = document.createElement('p');
+      pElement.textContent = word;
+      pElement.style.display = 'none'
+      element.appendChild(pElement);
+      parentDivElement.appendChild(element);
+   }
+
+   for (let word of words) {
+      createElement(word);
+   }
+
+   function onClick(event) {
+      if (event.target.nodeName == 'DIV')
+         event.target.querySelector('p').style.display = ''
+   }
 }
