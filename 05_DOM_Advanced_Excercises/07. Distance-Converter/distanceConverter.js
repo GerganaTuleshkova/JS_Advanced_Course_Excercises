fc2b2cebc0b1f6f01661809bed8ea01fed8fa331
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-06-02 20:45:34 +0300

    Problem 7 added

--- 05_DOM_Advanced_Excercises/07. Distance-Converter/distanceConverter.js
+++ 05_DOM_Advanced_Excercises/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,25 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+    let button = document.getElementById('convert');
+    button.addEventListener("click", calculate);
+
+    function calculate() {
+
+        let inputValue = Number(document.getElementById('inputDistance').value);
+        let inputUnit = document.getElementById('inputUnits').value;
+        let mapper = {
+            m: 1,
+            km: 0.001,
+            cm: 100,
+            mm: 1000,
+            mi: 0.000621371,
+            yrd: 1.09361,
+            ft: 3.28083,
+            in: 39.36996,
+        }
+
+        let outputUnit = document.getElementById('outputUnits').value;
+        let result = (inputValue / mapper[inputUnit]) * mapper[outputUnit];
+
+        document.getElementById('outputDistance').value = result;
+    }
 }
