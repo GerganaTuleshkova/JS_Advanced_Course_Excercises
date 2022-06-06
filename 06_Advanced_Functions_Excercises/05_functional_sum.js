e28e0bf243f30c41a5b26ddffa07f9f0fcbc6f60
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-06-06 16:25:11 +0300

    Problem 4  added

---
+++ 06_Advanced_Functions_Excercises/05_functional_sum.js
@@ -0,0 +1,18 @@
+function add(n) {
+    let sum = n;
+
+    function inner(number) {
+        sum += number;
+        console.log(sum, inner.toString())
+        return inner;
+    }
+
+    // inner.toString = () => {
+    //     console.log(sum)
+    //     return sum;
+    // }
+
+    return inner;
+}
+
+console.log(add(1)(6)(-3))
