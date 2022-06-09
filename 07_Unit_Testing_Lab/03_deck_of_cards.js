93a8736a5cbac4a11ee907b6425e6f99d943e474
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-06-09 19:51:10 +0300

    Updated

--- 07_Unit_Testing_Lab/03_deck_of_cards.js
+++ 07_Unit_Testing_Lab/03_deck_of_cards.js
@@ -1,3 +1,6 @@
+const cardFactory = require('./02_playing_cards');
+
+
 function printDeck(arrOfStrings) {
     let cards = [];
     for (let cardAsString of arrOfStrings) {
@@ -14,29 +17,29 @@ function printDeck(arrOfStrings) {
 
     console.log(cards.join(' '));
 
-    function cardFactory(faceGiven, suitGiven) {
-        const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-        const suits = {
-            S: '\u2660 ',
-            H: '\u2665 ',
-            D: '\u2666 ',
-            C: '\u2663 ',
-        }
+    // function cardFactory(faceGiven, suitGiven) {
+    //     const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+    //     const suits = {
+    //         S: '\u2660 ',
+    //         H: '\u2665 ',
+    //         D: '\u2666 ',
+    //         C: '\u2663 ',
+    //     }
     
-        if (!faces.includes(faceGiven) || (!suits[suitGiven])) {
-            throw new Error('Invalid face or suit!');
-        }
+    //     if (!faces.includes(faceGiven) || (!suits[suitGiven])) {
+    //         throw new Error('Invalid face or suit!');
+    //     }
     
-        let card = {
-            face: faceGiven,
-            suit: suits[suitGiven],
-            toString() {
-                return `${this.face}${this.suit}`
-            },
-        }
-        return card;
-    }
+    //     let card = {
+    //         face: faceGiven,
+    //         suit: suits[suitGiven],
+    //         toString() {
+    //             return `${this.face}${this.suit}`
+    //         },
+    //     }
+    //     return card;
+    // }
 }
 
 printDeck(['AS', '10D', 'KH', '2C']);
-printDeck(['5S', '3D', 'QD', '1C']);
+// printDeck(['5S', '3D', 'QD', '1C']);
