0de369cee7409e9010929301b66c78df41433d4b
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-06-09 19:50:51 +0300

    Updated

--- 07_Unit_Testing_Lab/02_playing_cards.js
+++ 07_Unit_Testing_Lab/02_playing_cards.js
@@ -1,5 +1,5 @@
 function cardFactory(faceGiven, suitGiven) {
-    const faces = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+    const faces = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
     const suits = {
         S: '\u2660 ',
         H: '\u2665 ',
@@ -20,5 +20,7 @@ function cardFactory(faceGiven, suitGiven) {
     }
     return card;
 }
+
+// module.exports = cardFactory;
 
 console.log(cardFactory('1', 'C').toString());
