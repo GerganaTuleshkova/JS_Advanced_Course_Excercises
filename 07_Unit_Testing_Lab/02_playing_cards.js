a6fe8eef6cf755ab5fa9982594ef9de83946a677
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-06-09 15:11:43 +0300

    Problem 2 added

---
+++ 07_Unit_Testing_Lab/02_playing_cards.js
@@ -0,0 +1,25 @@
+function cardFactory(faceGiven, suitGiven) {
+    const faces = [2, 3, 4, 5, 6, 7, 8, 9, 10, 'J', 'Q', 'K', 'A'];
+    const suits = {
+        S: '\u2660 ',
+        H: '\u2665 ',
+        D: '\u2666 ',
+        C: '\u2663 ',
+    }
+
+    if (!faces.includes(faceGiven) || (!suits[suitGiven])) {
+        throw new Error;
+        return;
+    }
+
+    let card = {
+        face: faceGiven,
+        suit: suits[suitGiven],
+        toString() {
+            return `${this.face}${this.suit}`
+        },
+    }
+    return card;
+}
+
+console.log(cardFactory('1', 'C').toString());
