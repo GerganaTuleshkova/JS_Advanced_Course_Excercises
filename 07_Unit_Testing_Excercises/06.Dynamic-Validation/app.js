e33b525c03ebb8d3dd4177489f13a56dcfd47ccd
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-06-10 16:50:47 +0300

    Problem 6 added

--- 07_Unit_Testing_Excercises/06.Dynamic-Validation/app.js
+++ 07_Unit_Testing_Excercises/06.Dynamic-Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    // TODO
+    let inputElement = document.getElementById('email')
+    inputElement.className = '';
+
+    inputElement.addEventListener('change', onChange);
+
+    function onChange(event) {
+        let regex = /[a-z]+@[a-z]+.[a-z]+/g;
+        let isValid = regex.exec(inputElement.value);
+        if (!isValid) {
+            inputElement.className = 'error';
+        } else {
+            inputElement.className = '';
+        }
+    }
 }
