00ba49ffe5886449ee9de6bfdf83757fc150140a
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-06-10 16:36:06 +0300

    Problem 5 added

--- 07_Unit_Testing_Excercises/05.Notification/app.js
+++ 07_Unit_Testing_Excercises/05.Notification/app.js
@@ -1,4 +1,13 @@
 function notify(message) {
-  console.log('todo')
-  // TODO:
+  let notificationElement = document.getElementById('notification');
+  let messageElement = document.createElement('p');
+  messageElement.textContent = message;
+  notificationElement.style.display = 'block'
+  notificationElement.appendChild(messageElement);
+
+  notificationElement.addEventListener('click', onMessageClick);
+
+  function onMessageClick(event) {
+    notificationElement.style.display = 'none';
+  }
 }
