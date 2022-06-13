7140f89327ccbb8d52c3ec78396ab8d601c50a94
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-06-13 11:44:21 +0300

    Problem 4 added

---
+++ 08_Classes_Excercises/04.List.js
@@ -0,0 +1,39 @@
+class List {
+    constructor() {
+        this.numbers = [];
+        // this.size = undefined;
+    }
+
+    add(element) {
+        this.numbers.push(element);
+        this.numbers.sort((a, b) => a - b);
+    }
+
+    remove(index) {
+        if (index >= 0 && index < this.size) {
+            this.numbers.splice(index, 1);
+        }
+    }
+
+    get(index) {
+        if (index >= 0 && index < this.size) {
+            return this.numbers[index];
+        }
+    }
+
+    get size() {
+        return this.numbers.length;
+    }
+}
+
+let list = new List();
+list.add(5);
+list.add(6);
+list.add(7);
+console.log(list.get(1));
+list.remove(1);
+console.log(list.get(1));
+console.log(list.size);
+console.log(list.hasOwnProperty('size'))
+
+console.log(Object.getOwnPropertyNames(list));
