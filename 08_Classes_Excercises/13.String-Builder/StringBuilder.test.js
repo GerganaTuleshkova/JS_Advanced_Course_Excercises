2799e9fd449b62fc93ed08b749b722fa43885c16
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-06-15 12:07:16 +0300

    Problem 13 added

---
+++ 08_Classes_Excercises/13.String-Builder/StringBuilder.test.js
@@ -0,0 +1,191 @@
+const { assert, expect } = require('chai');
+const { StringBuilder } = require('./StringBuilder');
+
+describe('Test StringBuilder Class', () => {
+
+    describe('instantianting tests', () => {
+        it('creates valid instance with valid aray when valid string is passed', () => {
+            let ns = new StringBuilder('str');
+            expect(ns._stringArray).to.eql(['s', 't', 'r']);
+        })
+
+        it('creates valid instance with empty array when no argument is passed', () => {
+            let ns = new StringBuilder();
+            expect(ns._stringArray).to.eql([]);
+        });
+
+        it('creates valid instance with empty array when empty string is passed', () => {
+            let ns = new StringBuilder('');
+            expect(ns._stringArray).to.eql([]);
+        });
+
+        it('throws error when arguments passed is not a string  ', () => {
+            expect(() => new StringBuilder(2)).to.throw('Argument must be a string');
+        });
+    })
+
+    describe('append tests', () => {
+        it('extends the array when valid string is passed', () => {
+            let ns = new StringBuilder('str');
+            ns.append('a');
+            expect(ns._stringArray).to.eql(['s', 't', 'r', 'a']);
+        });
+
+        it('does not change the array when empty string is passed', () => {
+            let ns = new StringBuilder('str');
+            ns.append('');
+            expect(ns._stringArray).to.eql(['s', 't', 'r']);
+        });
+
+        it('throws error when no argument is passed', () => {
+            let ns = new StringBuilder('str');
+            expect(() => ns.append()).to.throw('Argument must be a string');
+        });
+
+        it('throws error when argument passed is not a string  ', () => {
+            let ns = new StringBuilder('str');
+            expect(() => ns.append(2)).to.throw('Argument must be a string');
+        });
+    });
+
+    describe('prepend tests', () => {
+        it('extends the array when valid string is passed', () => {
+            let ns = new StringBuilder('str');
+            ns.prepend('an');
+            expect(ns._stringArray).to.eql(['a', 'n', 's', 't', 'r']);
+        });
+
+        it('does not change the array when empty string is passed', () => {
+            let ns = new StringBuilder('str');
+            ns.prepend('');
+            expect(ns._stringArray).to.eql(['s', 't', 'r']);
+        });
+
+        it('throws error when no argument is passed', () => {
+            let ns = new StringBuilder('str');
+            expect(() => ns.prepend()).to.throw('Argument must be a string');
+        });
+
+        it('throws error when argument passed is not a string  ', () => {
+            let ns = new StringBuilder('str');
+            expect(() => ns.prepend(2)).to.throw('Argument must be a string');
+        });
+    });
+
+    describe('insretAt tests', () => {
+        it('inserts in the array when valid string and index (0) are passed', () => {
+            let ns = new StringBuilder('str');
+            ns.insertAt('an', 0);
+            expect(ns._stringArray).to.eql(['a', 'n', 's', 't', 'r']);
+        });
+
+        it('inserts in the array when valid string and index (1) are passed', () => {
+            let ns = new StringBuilder('str');
+            ns.insertAt('an', 1);
+            expect(ns._stringArray).to.eql(['s', 'a', 'n', 't', 'r']);
+        });
+
+        it('inserts in the array when valid string and index (3) are passed', () => {
+            let ns = new StringBuilder('str');
+            ns.insertAt('an', 3);
+            expect(ns._stringArray).to.eql(['s', 't', 'r', 'a', 'n']);
+        });
+
+        it('inserts in the array when valid string and index more than lenght (4) are passed', () => {
+            let ns = new StringBuilder('str');
+            ns.insertAt('an', 4);
+            expect(ns._stringArray).to.eql(['s', 't', 'r', 'a', 'n']);
+        });
+
+        it('inserts in the array when valid string and negative index(-1) are passed', () => {
+            let ns = new StringBuilder('str');
+            ns.insertAt('an', -1);
+            expect(ns._stringArray).to.eql(['s', 't', 'a', 'n', 'r',]);
+        });
+
+        it('inserts in the array in the start when valid string and index as string are passed', () => {
+            let ns = new StringBuilder('str');
+            ns.insertAt('an', 'a');
+            expect(ns._stringArray).to.eql(['a', 'n', 's', 't', 'r',]);
+        });
+
+        it('inserts in the array in the start when valid string and no index are passed', () => {
+            let ns = new StringBuilder('str');
+            ns.insertAt('an');
+            expect(ns._stringArray).to.eql(['a', 'n', 's', 't', 'r',]);
+        });
+
+        it('does not change the array when empty string and valid index are passed', () => {
+            let ns = new StringBuilder('str');
+            ns.insertAt('', 2);
+            expect(ns._stringArray).to.eql(['s', 't', 'r',]);
+        });
+
+        it('throws error when no argument is passed', () => {
+            let ns = new StringBuilder('str');
+            expect(() => ns.insertAt()).to.throw('Argument must be a string');
+        });
+
+        it('throws error when non string is passed', () => {
+            let ns = new StringBuilder('str');
+            expect(() => ns.insertAt(2, 2)).to.throw('Argument must be a string');
+        });
+    });
+
+    describe('remove tests', () => {
+        it('removes the correct section from the array when valid index (0) and len (2) are passed', () => {
+            let ns = new StringBuilder('str');
+            ns.remove(0, 2);
+            expect(ns._stringArray).to.eql(['r']);
+        });
+
+        it('removes the correct section from the array when valid index (1) and len (2) are passed', () => {
+            let ns = new StringBuilder('str');
+            ns.remove(1, 2);
+            expect(ns._stringArray).to.eql(['s']);
+        });
+
+        it('removes nothing from the array when index is higher than lenght (4) and len (2) are passed', () => {
+            let ns = new StringBuilder('str');
+            ns.remove(4, 2);
+            expect(ns._stringArray).to.eql(['s', 't', 'r',]);
+        });
+
+        it('removes nothing from the array when index is negative (4) and len (2) are passed', () => {
+            let ns = new StringBuilder('str');
+            ns.remove(4, 2);
+            expect(ns._stringArray).to.eql(['s', 't', 'r',]);
+        });
+
+        it('removes a smaller section from the array when valid index (0) and len longer than length (5) are passed', () => {
+            let ns = new StringBuilder('str');
+            ns.remove(0, 5);
+            expect(ns._stringArray).to.eql([]);
+        });
+
+        it('removes a smaller section from the array when valid index (1) and len longer than length (5) are passed', () => {
+            let ns = new StringBuilder('str');
+            ns.remove(1, 5);
+            expect(ns._stringArray).to.eql(['s']);
+        });
+
+        it('removes nothing from the array when valid index (1) and negative len (-2) are passed', () => {
+            let ns = new StringBuilder('str');
+            ns.remove(1, -2);
+            expect(ns._stringArray).to.eql(['s', 't', 'r',]);
+        });
+
+        it('removes nothing from the array when valid index (1) and string len (a) are passed', () => {
+            let ns = new StringBuilder('str');
+            ns.remove(1, 'a');
+            expect(ns._stringArray).to.eql(['s', 't', 'r',]);
+        });
+    });
+
+    describe('toSring tests', () => {
+        it('returns correct result', () => {
+            let ns = new StringBuilder('str');
+            expect(ns.toString()).to.equal('str');
+        });
+    });
+});
