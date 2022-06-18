2b78a36e9983e2c774ef2c133402a8f28da91a11
GerganaTuleshkova <gergana.tuleshkova@gmail.com>
2022-06-18 15:19:43 +0300

    Reformatted

--- 10_Exam_Preparation/2002.04.06/02. Garden/mySolution.js
+++ 10_Exam_Preparation/2002.04.06/02. Garden/mySolution.js
@@ -22,12 +22,15 @@ class Garden {
     }
 
     ripenPlant(plantName, quantity) {
-        let searchedPlant;
-        for (let plant of this.plants) {
-            if (plant.plantName == plantName) {
-                searchedPlant = plant;
-            }
-        }
+        let searchedPlant = this.plants.find((x) => x.plantName == plantName);
+
+        // let searchedPlant;
+        // for (let plant of this.plants) {
+        //     if (plant.plantName == plantName) {
+        //         searchedPlant = plant;
+        //     }
+        // }
+
         if (searchedPlant == undefined) {
             throw new Error(`There is no ${plantName} in the garden.`);
         }
@@ -39,24 +42,26 @@ class Garden {
         if (quantity <= 0) {
             throw new Error('The quantity cannot be zero or negative.');
         }
-        
+
         searchedPlant.ripe = true;
         searchedPlant.quantity += quantity;
         if (quantity > 1) {
             return `${quantity} ${plantName}s have successfully ripened.`;
         } else {
             return `${quantity} ${plantName} has successfully ripened.`;
-        }     
+        }
     }
 
     harvestPlant(plantName) {
+        let searchedPlant = this.plants.find((x) => x.plantName == plantName);
 
-        let searchedPlant;
-        for (let plant of this.plants) {
-            if (plant.plantName == plantName) {
-                searchedPlant = plant;
-            }
-        }
+        // let searchedPlant;
+        // for (let plant of this.plants) {
+        //     if (plant.plantName == plantName) {
+        //         searchedPlant = plant;
+        //     }
+        // }
+        
         if (searchedPlant == undefined) {
             throw new Error(`There is no ${plantName} in the garden.`);
         }
@@ -67,7 +72,7 @@ class Garden {
 
         this.plants = this.plants.filter(plant => plant !== searchedPlant);
         let quantity = searchedPlant.quantity;
-        this.storage.push({plantName, quantity});
+        this.storage.push({ plantName, quantity });
         this.spaceAvailable += searchedPlant.spaceRequired;
         return `The ${plantName} has been successfully harvested.`;
     }
@@ -75,27 +80,22 @@ class Garden {
     generateReport() {
         let result = `The garden has ${this.spaceAvailable} free space left.`;
 
-        let sortedPlantsList = this.plants.map(pl => `${pl.plantName}`);
+        let sortedPlantsList = this.plants.map(plant => `${plant.plantName}`);
         sortedPlantsList = sortedPlantsList.sort((a, b) => (a).localeCompare(b));
 
         result += '\nPlants in the garden: ';
         result += sortedPlantsList.join(', ');
 
         if (this.storage.length == 0) {
-            result += 'Plants in storage: The storage is empty.'
+            result += '\nPlants in storage: The storage is empty.'
         } else {
             result += '\nPlants in storage: ';
 
             let storagePlantStringsList = this.storage.map(plant => `${plant.plantName} (${plant.quantity})`);
             result += storagePlantStringsList.join(', ');
-            // for (let plant of this.storage) {
-            //     result += `${plant.plantName} (${plant.quantity})`
-            // }
         }
-
         return result;
     }
-    
 }
 const myGarden = new Garden(250)
 console.log(myGarden.addPlant('apple', 20));
